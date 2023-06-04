ab8789f75f59c3adc0cbad9250a9640eb1513843
AyushmanBrijwal <pikachu00001raichu@gmail.com>
2023-06-04 21:49:35 +0530

    upd

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', Router);
 
-app.use(express.static(path.join("__dirname", "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get('*', function (_, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"), function(err) {
+        res.status(500).send(err);
+    })
+})
 
 const PORT = process.env.PORT || 8000;
 
